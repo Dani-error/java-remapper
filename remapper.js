8c166f018bd40f718761bb10e9b9b7c79645e6ab
Dani-error <zdani.error@gmail.com>
2025-07-27 13:52:59 +0100

    feat: js types

---
+++ remapper.js
@@ -0,0 +1,12 @@
+let remapFunc = null;
+
+export const remap = async (b, mappings) => {
+    if (!remapFunc) {
+        const { load } = await import("./runtime.js");
+        const { exports } = await load(new URL("./remapper.wasm", import.meta.url).href);
+
+        remapFunc = exports.remap;
+    }
+
+    return remapFunc(b, mappings);
+};
