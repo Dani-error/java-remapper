c29b7464d03015e49d4a8af592803646703f5056
Dani-error <zdani.error@gmail.com>
2025-07-27 13:53:58 +0100

    feat: dist package structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,3 +25,42 @@ teavm.wasmGC {
     mainClass = "dev.dani.remapper.Main"
     modularRuntime = true
 }
+
+tasks {
+    register<Copy>("copyDist") {
+        group = "build"
+        dependsOn(generateWasmGC)
+
+        from(
+            "README.md", "LICENSE", "remapper.js", "remapper.d.ts",
+            layout.buildDirectory.file("generated/teavm/wasm-gc/remapper.wasm"),
+            "wasm-runtime/runtime.js", "wasm-runtime/wasm-imports-parser.js"
+        )
+        into("dist")
+
+        doLast {
+            file("dist/package.json").writeText(
+                """
+                    {
+                      "name": "java-remapper",
+                      "version": "${project.version}",
+                      "description": "A JavaScript tool for remapping Java classes.",
+                      "main": "remapper.js",
+                      "types": "remapper.d.ts",
+                      "keywords": [
+                        "remapper",
+                        "java",
+                        "bytecode"
+                      ],
+                      "author": "Dani-error",
+                      "license": "MIT"
+                    }
+                """.trimIndent()
+            )
+        }
+    }
+
+    build {
+        dependsOn("copyDist")
+    }
+}
